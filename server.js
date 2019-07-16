39ea8e01721a9b64803c62beacd92e36c79e0414
Rafał Nowakowski <gee@vp.pl>
2019-07-16 22:22:45 +0200

    Add static files serve, form endpoint, userform endpoint

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-
 var app = express();
-var stringifyFile;
 
-app.use(bodyParser.json());
+app.use(express.static('assets'));
 
-app.get('/getNote', function (req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data;
-        res.send(data);
-    });
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    stringifyFile += req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-        if (err) throw err;
-        console.log('file updated');
-        res.send('file updated');
-    });
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response);
 });
 
-var server = app.listen(3000, function() {
-    console.log('Serwer nasłuchuje na http://localhost:3000');
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
