ad05b95998f332c888a9464bf582820018b29b97
Rafał Nowakowski <gee@vp.pl>
2019-07-16 22:50:44 +0200

    Add middleware function to /store endpoint

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('assets'));
-
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+app.use('/store', function(req, res, next){
+    console.log('Hej, jestem pośrednikiem przy żądaniu /store');
+    next();
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
 });
 
 var server = app.listen(3000, 'localhost', function() {
@@ -20,4 +19,8 @@ var server = app.listen(3000, 'localhost', function() {
     var port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
