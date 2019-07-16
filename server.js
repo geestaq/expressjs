2d66adb51e9b8a4f968ea48f1c94501d59e70ebf
Rafał Nowakowski <gee@vp.pl>
2019-07-16 23:37:17 +0200

    Add login simulation

--- server.js
+++ server.js
@@ -4,21 +4,16 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.use('/store', function(req, res, next){
-    console.log('Hej, jestem pośrednikiem przy żądaniu /store');
-    next();
-});
-
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('auth');
 });
 
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function(req, res){
-    res.render('first-template');
+app.get('/auth/google', function (req, res) {
+    res.render('auth', {
+        user: {
+            name: "geestaq"
+        }
+    });
 });
 
 var server = app.listen(3000, 'localhost', function() {
