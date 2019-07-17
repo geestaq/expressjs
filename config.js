57e28deba274956b0b9921bf105aed72452e2e79
Rafał Nowakowski <gee@vp.pl>
2019-07-17 08:12:54 +0200

    Add Google auth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "GOOGLE_CLIENT_ID": "1061047814590-sum3mhlv5rqkmp464rnlj9bre2mf488k.apps.googleusercontent.com",
+    "GOOGLE_CLIENT_SECRET": "oZWBP5wa45PXi47u1VF0Vduo",
+    "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+}
--- server.js
+++ server.js
@@ -1,28 +1,58 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+
 var app = express();
 
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret:config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
+
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', function (req, res) {
-    res.render('auth');
+//app routes
+app.get('/', function(req, res){
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('auth', {
-        user: {
-            name: "geestaq"
-        }
-    });
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
+//Passport routes
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    })
+);
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    })
+);
 
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
-
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+app.listen(3000);
